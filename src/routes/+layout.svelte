a66c1097846b3f26e5cf12146f60fe0d5ea89a9e
Wabosh <wabosh@svfe.de>
2023-03-24 23:13:26 +0100

    Add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
+	import '../app.css';
+
 	import NavigationLoader from '$lib/svelte/NavigationLoader.svelte';
 </script>
 
 <NavigationLoader animationDuration={1000} color="#fa8e0e" />
+<div class="w-screen flex">asd</div>
 <slot />
